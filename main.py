31f27ab6b6d5ded01cd95c426900517409ba117a
carolinazeniuk <carolinazeniuk9@gmail.com>
2023-07-06 18:00:49 +0300

    исправила main.py

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ import json
 content = get_json()
 last_five_operation = find_last_five(content)
 last_five = show_last_five(last_five_operation)
- # for i in last_five:
- #    print(i)
- #    print ("------")
-a = json.dumps(last_five_operation)
-print(a)
+for i in last_five:
+    print(i)
+    print("------")
 
 
 
